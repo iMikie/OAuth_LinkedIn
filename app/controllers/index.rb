cc5ebbe9519d5429c1877976287c1762d2cfce45
BootCoder <bootcoder@gmail.com>
2015-06-11 15:33:07 -0700

    i did some stuff for lecture -- people liked it

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -6,24 +6,68 @@ end
 
 #----------- SESSIONS -----------
 
-get '/sessions/new' do
-  # render sign-in page 
+get '/signin' do
+  erb :sign_in
 end
 
 post '/sessions' do
-  # sign-in
+  p '@' * 90
+  p params
+  p '@' * 90
+  @user = User.where(email: params[:email]).first if params[:email]
+  p '@' * 90
+  p @user
+  if @user.password_hash == params[:password]
+    session[:user_id] = @user.id
+    redirect '/'
+  else
+    @errors = @user.errors
+    p "your not very good at this"
+    erb :sign_in
+  end
 end
 
-delete '/sessions/:id' do
-  # sign-out -- invoked 
+delete '/signout' do
+  session[:user_id] = nil
+  redirect '/'
 end
 
 #----------- USERS -----------
 
-get '/users/new' do
-  # render sign-up page
+get '/signup' do
+  erb :sign_up
 end
 
 post '/users' do
-  # sign-up a new user
+
+  @user = User.new(params[:user])
+
+  # long hand version for clarity or if form fields passsed in dont match db fields....
+
+  # @user = User.new(name: params[:user][:name],
+  #                  email: params[:user][:email],
+  #                  password: params[:user][:password])
+
+
+  if @user.save
+    p "yaY!!!!!!"
+    session[:user_id] = @user.id
+    redirect '/'
+  else
+    @errors = @user.errors
+    p " duck " * 20
+    erb :sign_up
+  end
+
 end
+
+
+
+
+
+
+
+
+
+
+
--- app/helpers/sessions.rb
+++ app/helpers/sessions.rb
@@ -1,7 +1,7 @@
 helpers do
 
   def current_user
-        # TODO: return the current user if there is a user signed in.
+    @current_user ||= User.where(id: session[:user_id]).first if session[:user_id]
   end
 
 end
